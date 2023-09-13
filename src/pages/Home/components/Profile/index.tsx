f312c27571fb3a02de1c2dbb3489e8723825c199
DiogoKaster <diogokaster@gmail.com>
2023-09-13 16:55:36 -0300

    feat: finish Profile component

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -1,12 +1,87 @@
-import { ProfileContainer } from './styles'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { api } from '../../../../lib/axios'
+import {
+  ProfileContainer,
+  ProfileContent,
+  ProfileContentHeader,
+  ProfileDetails,
+} from './styles'
+import {
+  ArrowSquareOut,
+  GithubLogo,
+  Users,
+  Buildings,
+} from '@phosphor-icons/react'
+
+interface ProfileProps {
+  name: string
+  login: string
+  avatarUrl: string
+  bio: string
+  followers: number
+  githubUrl: string
+  company?: string
+}
 
 export function Profile() {
+  const [profile, setProfile] = useState<ProfileProps>({
+    name: '',
+    login: '',
+    avatarUrl: '',
+    bio: '',
+    followers: 0,
+    githubUrl: '',
+    company: '',
+  })
+
+  async function fetchProfile() {
+    const response = await api.get('/users/DiogoKaster')
+
+    setProfile({
+      name: response.data.name,
+      login: response.data.login,
+      avatarUrl: response.data.avatar_url,
+      bio: response.data.bio,
+      followers: response.data.followers,
+      githubUrl: response.data.html_url,
+      company: response.data.company,
+    })
+  }
+
+  useEffect(() => {
+    fetchProfile()
+  }, [])
+
   return (
     <ProfileContainer>
-      <h1>Image</h1>
-      <div>
-        <strong>Nome</strong>
-      </div>
+      <img src={profile.avatarUrl} alt={profile.name} />
+      <ProfileContent>
+        <ProfileContentHeader>
+          <h1>{profile.name}</h1>
+          <Link to={profile.githubUrl} target="_blank" rel="noreferrer">
+            GITHUB
+            <ArrowSquareOut size={20} />
+          </Link>
+        </ProfileContentHeader>
+        <p>{profile.bio}</p>
+        <ProfileDetails>
+          <span>
+            <GithubLogo weight="fill" size={22} />
+            {profile.login}
+          </span>
+          {profile.company && (
+            <span>
+              <Buildings weight="fill" size={22} />
+              {profile.company}
+            </span>
+          )}
+          <span>
+            <Users weight="fill" size={22} />
+            {profile.followers} followers
+          </span>
+        </ProfileDetails>
+      </ProfileContent>
     </ProfileContainer>
   )
 }
--- src/pages/Home/components/Profile/styles.ts
+++ src/pages/Home/components/Profile/styles.ts
@@ -12,4 +12,59 @@ export const ProfileContainer = styled.div`
   border-radius: 0.5rem;
   margin-top: -7rem;
   background: ${(props) => props.theme['base-profile']};
+
+  img {
+    width: 9.25rem;
+    height: 9.25rem;
+    flex-shrink: 0;
+    border-radius: 0.5rem;
+  }
+`
+
+export const ProfileContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.5rem;
+`
+
+export const ProfileContentHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.5rem;
+
+  h1 {
+    font-size: 1.5rem;
+    line-height: 130%;
+    color: ${(props) => props.theme['base-title']};
+  }
+
+  a {
+    display: inline-flex;
+    text-decoration: none;
+    gap: 0.5rem;
+    font-size: 0.75rem;
+    text-transform: uppercase;
+    color: ${(props) => props.theme.blue};
+  }
+`
+
+export const ProfileDetails = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  align-items: center;
+  margin-top: 1rem;
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    color: ${(props) => props.theme['base-subtitle']};
+
+    svg {
+      color: ${(props) => props.theme['base-label']};
+    }
+  }
 `
